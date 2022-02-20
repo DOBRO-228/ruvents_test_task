c33540c82e3f18557f889b163754b10eda1a2660
Dobro <matematikaigor@gmail.com>
2022-02-20 22:10:49 +0300

    finish part which works with numbers

--- xls_handler/numbers_calculator.py
+++ xls_handler/numbers_calculator.py
@@ -1,24 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.10
 
 """"""
 
 
-def calculate_even_numbers(numbers):
-    even_numbers = [number for number in numbers if not number % 2]
+def calculate_even_numbers(sheet, column):
+    all_column_values = list(map(lambda cell: cell.value, sheet[column]))
+    even_numbers = [number for number in all_column_values[2:] if not number % 2]
     return len(even_numbers)
 
 
+def calculate_prime_numbers(sheet, column):
+    all_column_values = list(map(lambda cell: cell.value, sheet[column]))
+    prime_numbers = [number for number in all_column_values[2:] if is_prime(number)]
+    return len(prime_numbers)
+
+
+def calculate_less_than_point_five_numbers(sheet, column):
+    all_column_values = list(map(lambda cell: cell.value, sheet[column]))
+    normal_numbers = standardize_numbers(all_column_values[2:])
+    less_than_five_numbers = [number for number in normal_numbers if number < 0.5]  # Noqa: WPS459
+    return len(less_than_five_numbers)
+
+
 def is_prime(number):
-    stop = int(number / 2)
-    for num in range(2, stop):
-        if number % num == 0:
+    start = int(number / 2)
+    for divisor in range(start, 1, -1):
+        if number % divisor == 0:
             return False
     return True
-
-
-def calculate_prime_numbers(numbers):
-    prime_numbers = [number for number in numbers if is_prime(number)]
-    return len(prime_numbers)
 
 
 def add_zero_if_has_not(number):
@@ -29,10 +38,4 @@ def standardize_numbers(numbers):
     without_spaces = map(lambda num_as_str: num_as_str.replace(' ', ''), numbers)
     with_dots = map(lambda num_as_str: num_as_str.replace(',', '.'), without_spaces)
     normal_numbers_as_str = map(add_zero_if_has_not, with_dots)
-    return map(float, normal_numbers_as_str)
-
-
-def calculate_less_than_five_numbers(numbers):
-    normal_numbers = standardize_numbers(numbers)
-    less_than_five_numbers = [number for number in numbers if number < 0.5]  # Noqa: WPS459
-    return len(less_than_five_numbers)
+    return list(map(float, normal_numbers_as_str))
--- xls_handler/xls_handler.py
+++ xls_handler/xls_handler.py
@@ -1,8 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.10
 
 """"""
 
+from openpyxl import load_workbook
 import argparse
+import sys
+from xls_handler.numbers_calculator import (
+    calculate_even_numbers,
+    calculate_prime_numbers,
+    calculate_less_than_point_five_numbers,
+)
+
+
+def calculate(filepath=None):
+    excel_file = load_workbook(filepath)
+    sheet = excel_file['Tasks']
+    sys.stdout.write('There are {0} even numbers\n'.format(calculate_even_numbers(sheet, 'B')))
+    sys.stdout.write('There are {0} prime numbers\n'.format(calculate_prime_numbers(sheet, 'C')))
+    sys.stdout.write(
+        'There are {0} numbers which less than point five\n'.format(calculate_less_than_point_five_numbers(sheet, 'D')),
+    )
 
 
 def main():
@@ -14,10 +31,17 @@ def main():
         add_help=False,
     )
     parser.add_argument(
-        'first_file',
+        'filepath',
         help=argparse.SUPPRESS,
     )
-    return 1
+    parser.add_argument(
+        '-h',
+        '--help',
+        action='help',
+        help='display help for command',
+    )
+    args = parser.parse_args()
+    calculate(args.filepath)
 
 
 if __name__ == '__main__':
